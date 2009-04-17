cde05a52ca9f77dd4453f023ce7ae8f85f87a8c1
Joe Ferris <joe.r.ferris@gmail.com>
2009-04-17 10:34:29 -0700

    Added a basic implementation of test spies

--- lib/mocha/expectation.rb
+++ lib/mocha/expectation.rb
@@ -435,8 +435,9 @@ module Mocha # :nodoc:
       @cardinality.satisfied?(@invocation_count)
     end
   
-    def invoke
+    def invoke(args)
       @invocation_count += 1
+      Mockery.instance.invocation(@mock, method_name, args)
       perform_side_effects()
       if block_given? then
         @yield_parameters.next_invocation.each do |yield_parameters|
@@ -471,6 +472,12 @@ module Mocha # :nodoc:
     
     def method_signature
       "#{@mock.mocha_inspect}.#{@method_matcher.mocha_inspect}#{@parameters_matcher.mocha_inspect}"
+    end
+
+    private
+
+    def method_name
+      @method_matcher.expected_method_name
     end
       
     # :startdoc:
